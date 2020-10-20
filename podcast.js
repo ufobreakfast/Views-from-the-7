7fc4c6c29fe3b3511b329ff7b0b3710409afa572
ufobreakfast <gmealy26@gmail.com>
2020-10-20 01:00:06 -0700

    Add files via upload

---
+++ podcast.js
@@ -0,0 +1,10 @@
+// custom scroll bar 
+let progress = document.getElementById('progressBar')
+let totalHeight = document.body.scrollHeight - window.innerHeight
+window.onscroll = function() {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100
+  progress.style.height = progressHeight + '%'
+}
+// scroll end 
+
+// test 
